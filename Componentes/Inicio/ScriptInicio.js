a35c53812f451713a8393cd680bb29a2408de914
AdrianoGoede <43224315+AdrianoGoede@users.noreply.github.com>
2020-09-25 10:52:56 -0300

    Correções e melhorias na interface

--- Componentes/Inicio/ScriptInicio.js
+++ Componentes/Inicio/ScriptInicio.js
@@ -4,12 +4,21 @@ const AlternarPaginaInicio = () => {
 
 window.onload = () => {
     AlternarPaginaInicio();
+	if (navigator.userAgent.includes("Chrome"))
+		ResultadoConsultaNumero.rows = 8;
 }
 
 $("#Sobre").click(() => {
-    $("title").html("!!! Sobre !!!");
-    alert("Desenvolvido por Adriano Andrei Goede\n\n         Microsoft Visual Studio Code\n\n             Debian 10.5 GNU/Linux");
-    $("title").html("Bem Vindo(a)!");
+    if (navigator.userAgent.includes("Firefox")) {
+        $("title").html("!!! Sobre !!!");
+        alert("Desenvolvido por Adriano Andrei Goede\n\n         Microsoft Visual Studio Code\n\n             Debian 10.5 GNU/Linux");
+        $("title").html("Bem Vindo(a)!");
+    }
+    else if (navigator.userAgent.includes("Chrome")) {
+        $("title").html("!!! Sobre !!!");
+        alert("Desenvolvido por Adriano Andrei Goede\nMicrosoft Visual Studio Code\nDebian 10.5 GNU/Linux");
+        $("title").html("Bem Vindo(a)!");
+    }
 });
 
 $("#Inserir").click(() => {
